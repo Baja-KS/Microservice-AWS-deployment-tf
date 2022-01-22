87aebd6b0748e18269c02c2fca92118768dd0199
Baja-KS <lukic.l210@gmail.com>
2022-01-22 22:08:01 +0100

    load balancer added

--- roots/webshopapi-auth-svc/main.tf
+++ roots/webshopapi-auth-svc/main.tf
@@ -6,6 +6,13 @@ terraform {
   }
 }
 
+provider "aws" {
+  profile    = "default"
+  region     = var.aws_region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
 module "auth-svc-pipeline" {
   source                 = "../../modules/codepipeline"
   ecr_repo               = var.ecr_repo
@@ -22,4 +29,67 @@ module "auth-svc-pipeline" {
   aws_access_key_id      = var.aws_access_key_id
   aws_secret_access_key  = var.aws_secret_access_key
 }
+
+
+module "vpc" {
+  source                = "../../modules/vpc"
+  aws_region            = var.aws_region
+  aws_access_key_id     = var.aws_access_key_id
+  aws_secret_access_key = var.aws_secret_access_key
+}
+
+module "alb" {
+  source                = "../../modules/lb"
+  aws_region            = var.aws_region
+  aws_access_key_id     = var.aws_access_key_id
+  aws_secret_access_key = var.aws_secret_access_key
+  vpc_id                = module.vpc.vpc_id
+  subnet_id             = module.vpc.subnet_id
+  subnet2_id            = module.vpc.subnet2_id
+}
+
+module "api_gateway" {
+  source                = "../../modules/api-gateway"
+  aws_region            = var.aws_region
+  aws_access_key_id     = var.aws_access_key_id
+  aws_secret_access_key = var.aws_secret_access_key
+  lb_url                = module.alb.lb_dns
+}
+
+resource "aws_ecs_cluster" "foo" {
+  name = "test-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+
+
+
+
+resource "aws_network_interface" "eth" {
+  subnet_id   = module.vpc.subnet_id
+  private_ips = ["10.0.0.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "test" {
+  ami           = "ami-0015a39e4b7c0966f" # us-west-2
+  instance_type = "t2.micro"
+
+
+  network_interface {
+    network_interface_id = aws_network_interface.eth.id
+    device_index         = 0
+  }
+
+
+}
+
+
 
